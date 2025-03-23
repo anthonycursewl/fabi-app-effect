6f067abeb6824e73e179f066c56dc486c4e9532a
Anthony Cursewl <zerpaanthony.wx@gmail.com>
2025-03-23 18:23:31 -0400

    *

---
+++ app/ui/dashboard/components/FirstStep/FirstStepMain.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native"
+import TextWithColor from "@/app/shared/components/TextWithColor"
+
+export default function FirstStepMain() {
+    return (
+        <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+            <TextWithColor>FirstStepMain</TextWithColor>
+        </View>
+    )
+}
