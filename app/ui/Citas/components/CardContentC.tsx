51fbab0efb81c297bfeffbbddc5b75d30d893da3
Anthony Cursewl <zerpaanthony.wx@gmail.com>
2025-04-04 18:43:14 -0400

    *

--- app/ui/Citas/components/CardContentC.tsx
+++ app/ui/Citas/components/CardContentC.tsx
@@ -1,4 +1,4 @@
-import { View, TouchableOpacity, Modal } from "react-native"
+import { View, TouchableOpacity, Modal, Alert } from "react-native"
 import TextWithColor from "@/app/shared/components/TextWithColor"
 import { RenderTypeStatus } from "./RenderTypeStatus"
 import { useState } from "react"
@@ -12,7 +12,16 @@ export const CardContentC = ({ item }: { item: any }): JSX.Element => {
         console.log('Long press detected here')
         setIsSelected(!isSelected)
     }
-    
+
+    const handleDelete = () => {
+        Alert.alert('Eliminar cita', 
+            '¿Estas seguro de querer eliminar esta cita?', 
+            [{ text: 'Si', 
+            onPress: () => console.log('Si') }, 
+            { text: 'No', onPress: () => console.log('No') }
+        ])
+    }
+     
     return (
         <>
         <TouchableOpacity
@@ -44,15 +53,34 @@ export const CardContentC = ({ item }: { item: any }): JSX.Element => {
                         <TextWithColor>{item.contdr_profile.users.name}</TextWithColor>
                     </View>
                 </View>
-        </View>
+            </View>
         </TouchableOpacity>
 
-        // If you're read this, let me tell you that I love u u-u. Don't complain bro just love xoxoxo
+        {/* // If you're read this, let me tell you that I love u u-u. Don't complain bro just love xoxoxo */}
 
         <Modal visible={isSelected} onRequestClose={() => setIsSelected(!isSelected)}
-        animationType="slide" transparent>
-            <View style={{ width: '100%', height: '100%', backgroundColor: 'rgba(0, 0, 0, 0.5)', alignItems: 'center', justifyContent: 'center' }}>
-                <TextWithColor>Modal aqui {item.des_or_reason}</TextWithColor>
+        animationType="fade" transparent
+        >
+            <View style={{ flex: 1, justifyContent: 'flex-end', alignItems: 'center', backgroundColor: 'rgba(0, 0, 0, 0.5)'}}
+            >
+                
+                <View style={{ backgroundColor: 'white', padding: 20, borderRadius: 10, width: '100%', height: 'auto', alignItems: 'center', justifyContent: 'flex-start' }}>
+                    <TextWithColor color="black" style={{ fontSize: 18 }}>{item.des_or_reason}</TextWithColor>
+
+                    <View style={{ width: '100%', flexDirection: 'row', justifyContent: 'space-between'}}>
+                        <TextWithColor>{item.hour}</TextWithColor>
+                        <TextWithColor>{date.toLocaleDateString()}</TextWithColor>
+                        <RenderTypeStatus status={item.status}/>
+                    </View>
+
+                    <View style={{ width: '100%', flexDirection: 'row', justifyContent: 'space-between', marginTop: 15 }}>
+                        <TouchableOpacity style={{ backgroundColor: 'red', padding: 10, borderRadius: 10, width: '100%', alignItems: 'center' }}
+                        onPress={handleDelete}
+                        >
+                            <TextWithColor style={{ color: 'white', fontSize: 16 }}>Eliminar</TextWithColor>
+                        </TouchableOpacity>
+                    </View>
+                </View>
             </View>
         </Modal>
         </>
