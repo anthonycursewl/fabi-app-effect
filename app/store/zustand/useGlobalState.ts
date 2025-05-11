f96f5027639b57fb4c68ec08fa8b51239d483df2
Anthony Cursewl <zerpaanthony.wx@gmail.com>
2025-05-10 20:31:26 -0400

    [change] global state central user

--- app/store/zustand/useGlobalState.ts
+++ app/store/zustand/useGlobalState.ts
@@ -1,10 +1,18 @@
 
+import { IUserProfile } from "@/app/shared/interfaces/User";
 import { create } from "zustand";
 
 interface IGlobalState {
-    SuMarroz: string
+    user: IUserProfile, 
+    setUser: (user: IUserProfile) => void
 }
 
 export const useGlobalState = create<IGlobalState>((_set) => ({
-    SuMarroz: '',
+    user: { 
+        id: '', username: '', 
+        email: '', password: '', 
+        created_at: '', role: '', 
+        name: '', icon_url: '' 
+    },
+    setUser: (user: IUserProfile) => _set({ user })
 }));
